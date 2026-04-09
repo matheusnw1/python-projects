baf9ff73ef1a9c7d73c323a55bb8c0840b7a462f
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-04-08 23:53:35 -0300

    Update spot.py

--- spot.py
+++ spot.py
@@ -1,1 +1,20 @@
 
+import requests
+
+musica = requests.get('https://itunes.apple.com/search?term=ogrilo&media=music&limit=5')
+
+dicionario = (musica.json())
+
+musicas = (dicionario['results'])
+
+decisao = input('Qual artista você quer ouvir?: ')
+# if decisao:
+
+
+
+
+i = 1
+
+for musica in musicas:
+    print(f'Musica {i}:  {musica['trackName']}')
+    i += 1
