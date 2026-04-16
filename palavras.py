1a0930d1cec448187fd4e880b05e322cc7d4cd63
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-04-16 19:50:23 -0300

    Update palavras.py

--- palavras.py
+++ palavras.py
@@ -1,1 +1,34 @@
 
+palavras = [
+    "arara",
+    "banana",
+    "ovo",
+    "python",
+    "radar",
+    "nivel"
+]
+
+print("Palavras:")
+for p in palavras:
+    print(p)
+
+palindromos = []
+nao_palindromos = []
+
+for p in palavras:
+    if p == p[::-1]:
+        palindromos.append(p)
+    else:
+        nao_palindromos.append(p)
+
+quantidade = len(palindromos)
+
+maior_palindromo = ""
+for p in palindromos:
+    if len(p) > len(maior_palindromo):
+        maior_palindromo = p
+
+print("\nPalíndromos:", palindromos)
+print("Quantidade:", quantidade)
+print("Maior palíndromo:", maior_palindromo)
+print("Não palíndromos:", nao_palindromos)
