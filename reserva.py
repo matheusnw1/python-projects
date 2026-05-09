15ab7c0b850134007d1bcde5c15ef8167358e83f
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-05-08 22:30:55 -0300

    Update reserva.py

--- reserva.py
+++ reserva.py
@@ -1,1 +1,29 @@
 
+assentos = [1, 2, 5, 8, 10, 15]
+
+total_assentos = 20
+
+livres = []
+
+for i in range(1, total_assentos + 1):
+    if i not in assentos:
+        livres.append(i)
+
+print("Assentos ocupados:", assentos)
+print("Assentos livres:", livres)
+
+reserva = 7
+
+if reserva < 1 or reserva > total_assentos:
+    print("Assento inválido")
+elif reserva in assentos:
+    print("Assento já ocupado")
+else:
+    assentos.append(reserva)
+    assentos.sort()
+    print("Reserva realizada")
+
+ocupacao = (len(assentos) / total_assentos) * 100
+
+print("\nAssentos atualizados:", assentos)
+print(f"Taxa de ocupação: {ocupacao:.1f}%")
