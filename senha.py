27757cf332222ee63e8045871de43b025ebb971b
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-05-18 23:32:04 -0300

    Update senha.py

--- senha.py
+++ senha.py
@@ -1,1 +1,25 @@
 
+import random
+import string
+
+letras = string.ascii_letters
+numeros = string.digits
+simbolos = string.punctuation
+
+todos = letras + numeros + simbolos
+
+quantidade = int(input("Quantidade de caracteres: "))
+
+senha = "".join(random.choice(todos) for _ in range(quantidade))
+
+tem_numero = any(c in numeros for c in senha)
+tem_simbolo = any(c in simbolos for c in senha)
+
+while not (tem_numero and tem_simbolo):
+
+    senha = "".join(random.choice(todos) for _ in range(quantidade))
+
+    tem_numero = any(c in numeros for c in senha)
+    tem_simbolo = any(c in simbolos for c in senha)
+
+print(f"Senha gerada: {senha}")
